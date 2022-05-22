0cf2a50c208c3ac754f01b6525d52000c7dc766a
unknown <https://github.com/Pooja-bhaskaran/Day03.git>
2022-05-22 22:28:01 +0530

    The player position

--- src/com/Bridgelabz/SnakeAndLadder.java
+++ src/com/Bridgelabz/SnakeAndLadder.java
@@ -3,9 +3,7 @@ package com.Bridgelabz;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("***** Snake and Ladder Game *****");
-        int Position = 0;
-        int dieRoll =(int) (Math.random()*6)+1;
-        System.out.println("The Game Starts at position number: " + Position);
-        System.out.println("The dieRoll value is : " + dieRoll);
+        int position = 0;
+        System.out.println("The Game Starts at Position number: " + position);
     }
 }
