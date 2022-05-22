9023f735d5327107258e81fa4bc22ad6da1eea91
unknown <https://github.com/Pooja-bhaskaran/Day03.git>
2022-05-22 21:43:53 +0530

    Snake and Ladder Game

---
+++ src/com/Bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package com.Bridgelabz;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("***** Snake and Ladder Game *****");
+        int Position = 0;
+        System.out.println("The Game Starts at position number: " + Position);
+    }
+}
