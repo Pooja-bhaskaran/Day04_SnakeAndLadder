0a924bb5359645d6f87e519069cd7db692a899f6
unknown <https://github.com/Pooja-bhaskaran/Day03.git>
2022-05-23 16:45:16 +0530

    The Winner of Snake and Ladder

--- src/com/Bridgelabz/SnakeAndLadder.java
+++ src/com/Bridgelabz/SnakeAndLadder.java
@@ -2,42 +2,68 @@ package com.Bridgelabz;
 
 import java.util.Random;
 
-import static java.lang.Math.random;
-
 public class SnakeAndLadder {
 
     static final int NO_PLAY = 1;
     static final int LADDER = 2;
     static final int SNAKE = 3;
+    static int diceCount = 0;
+    static final int PLAYER1 = 1;
+    static final int PLAYER2 = 2;
 
-    static int diceCount=0;
+    public static int positionCheck(int dice,int CurrentPosition, int optionCheck) {
+        if ((optionCheck == LADDER) && (CurrentPosition + dice) <= 100) {
+            System.out.print(" ***** ladder **** ");
+            CurrentPosition = CurrentPosition + dice;
+        } else if (optionCheck == SNAKE) {
+            System.out.print("!!!!!! snake !!!!! ");
+            CurrentPosition = CurrentPosition - dice;
+        } else {
+            System.out.print(" /////  NoPlay ///// ");
+            CurrentPosition = CurrentPosition;
+        }
+        if (CurrentPosition < 0) {
+            CurrentPosition = 0;
+        }
+        System.out.println("CurrentPosition : " + CurrentPosition);
+        return CurrentPosition;
+    }
 
     public static void main(String[] args) {
-        int position = 0;
-        System.out.println("positionCheck: " + position);
-//        Random random = new Random();
+        int playerOnePosition = 0;
+        int playerTwoPosition = 0;
+        int player = PLAYER1;
+        System.out.println("playerOnePosition : " + playerOnePosition);
+        System.out.println("playerTwoPosition : " + playerTwoPosition);
+        Random ran = new Random();
 
-        while(position < 100) {
-            int diceRoll = (int) (random() * 6) + 1;
-            System.out.println("dice: " + diceRoll);
-            diceCount++;
-            int option = (int) (random() * 3)+1;
-            System.out.println("optionCheck: " + option);
+        while ((playerOnePosition < 100) && (playerTwoPosition < 100)) {
 
-            if ((option == LADDER) && (position + diceRoll) <= 100) {
-                System.out.println("!! LADDER !! The player will move ahead");
-                position = position + diceRoll;
-            } else if (option == SNAKE) {
-                System.out.println("!! SNAKE !! The player position will be reduced");
-                position = position - diceRoll;
-            } else {
-                System.out.println("!! NO_PLAY !! The player will remain in the Same Position");
+            int dice = ran.nextInt(6) + 1;
+            System.out.println("dice : " + dice);
+            ++diceCount;
+            int optionCheck = ran.nextInt(3) + 1;
+            System.out.println("optionCheck : " + optionCheck);
+
+            if (player == PLAYER1) {
+                playerOnePosition = positionCheck(dice, playerOnePosition, optionCheck);
+                if ((optionCheck == SNAKE) || (optionCheck == NO_PLAY)) {
+                    player = PLAYER2;
+                }
+            } else if (player == PLAYER2) {
+                playerTwoPosition = positionCheck(dice, playerTwoPosition, optionCheck);
+                if ((optionCheck == SNAKE) || (optionCheck == NO_PLAY)) {
+                    player = PLAYER1;
+                }
             }
-            if (position < 0) {
-                System.out.println("The player should restart from 0th position");
-                position = 0;
-            }
-            System.out.println("current position: " + position + "\n diceCount: " + diceCount);
+        }
+        System.out.println("PlayerOnePosition : " + playerOnePosition);
+        System.out.println("PlayerTwoPosition : " + playerTwoPosition);
+        System.out.println("diceCount: " + diceCount);
+        if (playerOnePosition == 100) {
+            System.out.println("Player_1 Wins");
+        } else {
+            System.out.println("Player_2 Wins");
         }
     }
 }
